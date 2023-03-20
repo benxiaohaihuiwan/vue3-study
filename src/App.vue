b303f5ec94d692443749c1df65b20c819e569f90
22017147 <ltc,.111992>
2023-03-20 09:54:51 +0800

    fix： layout布局aside

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <Login />
+  <!-- <Login /> -->
+  <Layout />
 </template>
 <script setup lang="ts">
 import { defineAsyncComponent } from 'vue'
 // 引入组件
-const Login = defineAsyncComponent(() => import('@/views/login/index.vue'))
+// const Login = defineAsyncComponent(() => import('@/views/login/index.vue'))
+const Layout = defineAsyncComponent(() => import('@/layout/index.vue'))
 </script>
 <style scoped lang="scss">
 </style>
