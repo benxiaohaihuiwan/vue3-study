d0edd9f5a3ffa4db480667a9df8b64f78c33640f
22017147 <ltc,.111992>
2023-03-20 09:54:02 +0800

    feature： 增加home首页

---
+++ src/views/home/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>12345</div>
+</template>
+
+<script setup lang='ts' name=''>
+import { ref, reactive, onMounted, computed } from 'vue'
+</script>
+
+<style scoped lang='scss'>
+</style>
