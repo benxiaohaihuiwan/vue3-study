4b257f734c69d717fbf7721f66167a0785fed579
22017147 <ltc,.111992>
2023-03-20 20:53:37 +0800

    fix: 增加布局左侧菜单里的第第二级菜单显示

---
+++ src/layout/navMenu/subItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <template v-for="val in chils">
+    <el-sub-menu
+      :index="val.path"
+      :key="val.path"
+      v-if="val.children && val.children.length"
+    >
+      <template #title>
+        <SvgIcon :name="val.meta.icon"></SvgIcon>
+        <span>{{ $t(val.meta.title) }}</span>
+      </template>
+      <!-- 组件自用 -->
+      <sub-item :chil="val.children"></sub-item>
+    </el-sub-menu>
+    <template v-else>
+      <el-menu-item :index="val.path" :key="val.path">
+        <template
+          v-if="!val.meta.isLink || (val.meta.isLink && val.meta.isIframe)"
+        >
+          <SvgIcon :name="val.meta.icon"></SvgIcon>
+          <span>{{ $(val.meta.title) }}</span>
+        </template>
+        <template v-else>
+          <a class="w100" @click.prevent="onALinkClick(val)">
+            <SvgIcon :name="val.meta.icon"></SvgIcon>
+            {{ $t(val.meta.title) }}
+          </a>
+        </template>
+      </el-menu-item>
+    </template>
+  </template>
+</template>
+
+</template>
+
+<script setup lang='ts' name=''>
+import { computed } from 'vue'
+import { RouteRecordRaw } from 'vue-router'
+import other from '@/utils/other'
+
+// 定义父组件传过来的值
+const props = defineProps({
+  // 菜单列表
+  chil: {
+    type: Array<RouteRecordRaw>,
+    default: () => [],
+  },
+})
+
+// 获取父级菜单数据
+const chils = computed(() => {
+  return <RouteItems>props.chil
+})
+
+// 打开外部链接
+const onALinkClick = (val: RouteItem) => {
+  other.handleOpenLink(val)
+}
+</script>
+
+<style scoped lang='scss'>
+</style>
